cad564db6891b53398cc111aff1b7b6bc1690523
Wisha Putra Maulana <wishaputramaulana@gmail.com>
2026-07-21 08:58:07 +0700

    fix: correct namespace for BelongsToCompany trait in ActivityLog model

--- app/Models/ActivityLog.php
+++ app/Models/ActivityLog.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Models\Traits\BelongsToCompany;
+use App\Traits\BelongsToCompany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
